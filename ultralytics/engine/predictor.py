a305f04588c65301da1b5852d61e585da5bae076
Mohammad Shoaei <mshoaei@proton.me>
2023-11-06 07:47:48 +0330

    multi-channel support

--- ultralytics/engine/predictor.py
+++ ultralytics/engine/predictor.py
@@ -240,7 +240,7 @@ class BasePredictor:
 
         # Warmup model
         if not self.done_warmup:
-            self.model.warmup(imgsz=(1 if self.model.pt or self.model.triton else self.dataset.bs, 3, *self.imgsz))
+            self.model.warmup(imgsz=(1 if self.model.pt or self.model.triton else self.dataset.bs, self.model.model.yaml.get("ch", 3), *self.imgsz))
             self.done_warmup = True
 
         self.seen, self.windows, self.batch, profilers = 0, [], None, (ops.Profile(), ops.Profile(), ops.Profile())
